97d9dc8dcb50f9e4f6e390c56622c068461f712a
ozruitincyyte <41006540+ozruitincyyte@users.noreply.github.com>
2018-07-09 17:44:28 +0100

    Add files via upload
    initial commit

---
+++ src/test/java/com/incyyte/cloud/miawebclient/MiawebClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.incyyte.cloud.miawebclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MiawebClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
